019b99977658ec81b3a8635aa83dd8eed9c5327f
Jose M. Domínguez <jmdomher@telefonica.net>
2024-09-02 09:37:20 +0200

    Commit primero

---
+++ ProyectoPruebaWindowsGIT/src/main/java/gmail/jose/manuel/domin/Main.java
@@ -0,0 +1,14 @@
+package gmail.jose.manuel.domin;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			
+			System.out.println("Iteración: " + i);
+			
+		}
+
+	}
+
+}
